f7503d974eefa16e528b1a88ae80e202754e55cb
Magno Silva <131492865+Magnols@users.noreply.github.com>
2025-06-27 18:19:45 -0300

    Estruturando o projeto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+
+import Header from './components/Header'; // 1. Importamos nosso componente
+
+function App() {
+  return (
+    <div>
+      <Header /> {/* 2. Usamos nosso componente aqui */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// src/components/Header.jsx
+
+function Header() {
+  return (
+    <header>
+      <h1>Magno [Seu Sobrenome]</h1>
+      <nav>
+        <a href="#sobre">Sobre</a>
+        <a href="#projetos">Projetos</a>
+        <a href="#contato">Contato</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
